d40f6d8d65267b862387db169e3efbb5422f1def
Hamza Nachite <76076322+hamzanachit@users.noreply.github.com>
2024-07-15 22:06:21 +0100

    fix register and table users to user

--- module/Application/src/Controller/Factory/AuthenticationServiceFactory.php
+++ module/Application/src/Controller/Factory/AuthenticationServiceFactory.php
@@ -13,7 +13,7 @@ class AuthenticationServiceFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         $dbAdapter = $container->get('Laminas\Db\Adapter\Adapter');
-        $authAdapter = new CredentialTreatmentAdapter($dbAdapter, 'users', 'username', 'password', 'SHA1(?)');
+        $authAdapter = new CredentialTreatmentAdapter($dbAdapter, 'user', 'username', 'password', 'SHA1(?)');
 
         $authService = new AuthenticationService();
         $authService->setAdapter($authAdapter);
