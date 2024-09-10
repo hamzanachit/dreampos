d9ff2cbf18438acd147ea0866f2e9fa6a3775d94
Hamza Nachite <76076322+hamzanachit@users.noreply.github.com>
2024-09-10 18:08:28 +0100

    Update global.php

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,8 +12,68 @@ use App\Doctrine\DBAL\Types\EnumType;
  * control, so do not include passwords or other sensitive information in this
  * file.
  */
-
+ 
+ 
 return [
-    // ...
+    'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => [
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'root',
+                    'password' => '',
+                    'dbname'   => 'erp',
+                    'charset'  => 'utf8mb4', // Adjust charset if necessary
+                ],
+            ],
+        ],
+        'driver' => [
+            'application_entities' => [
+                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [__DIR__ . '/../../module/Application/src/Entity'],
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    'Application\Entity' => 'application_entities',
+                ],
+            ],
+        ],
+    ],
+    'db' => [
+        'driver'         => 'Pdo',
+        'dsn'            => 'mysql:dbname=erp;host=localhost',
+        'username'       => 'root',
+        'password'       => '',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ],
+    ],
+    'service_manager' => [
+        'factories' => [
+            Laminas\Db\Adapter\Adapter::class => Laminas\Db\Adapter\AdapterServiceFactory::class,
+        ],
+    ],
+     'session_config' => [
+        'cookie_lifetime' => 60 * 60 * 1, // 1 hour
+        'gc_maxlifetime' => 60 * 60 * 24 * 30, // 30 days
+    ],
+    'session_manager' => [
+        'validators' => [
+            \Laminas\Session\Validator\RemoteAddr::class,
+            \Laminas\Session\Validator\HttpUserAgent::class,
+        ],
+    ],
+    'session_storage' => [
+        'type' => \Laminas\Session\Storage\SessionArrayStorage::class,
+    ],
+    'session_containers' => [
+        'UserSession',
+    ],
 ];
+
+// Register the custom enum type
+Type::addType('enum', EnumType::class);
 Type::addType(EnumType::ENUM_TYPE, EnumType::class);
